e9a0266f08b044b7ad5e6a54732085b7bfaf5c47
marvelsofmas <108832774+marvelsofmas@users.noreply.github.com>
2023-11-21 16:12:18 -0600

    added base robot methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClose.java
@@ -1,14 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
 @Autonomous
-public class IsaaqRR extends LinearOpMode {
+public class AutonClose extends LinearOpMode {
     double startposx = 0;
     double startposy = 0;
     double startheading = Math.toRadians(90);
