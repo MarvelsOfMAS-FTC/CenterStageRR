97789eb84232735eaff4031eb0744a52f27e9c55
marvelsofmas <108832774+marvelsofmas@users.noreply.github.com>
2023-11-21 18:15:06 -0600

    camera working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClose.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -7,26 +6,42 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous
 public class AutonClose extends LinearOpMode {
-
     //VARIABLES---------------------------------------------------------------------------------------------------------------
-    double startposx = 0;
-    double startposy = 0;
+    double startposx = 12;
+    double startposy = 72;
     double startheading = Math.toRadians(90);
+    double tagposx=0;
+    double tagposy=0;
+
     @Override
     public void runOpMode() throws InterruptedException {
-
         //ROBOT INITIALIZATION ---------------------------------------------------------------------
+        BaseRobotMethods robot = new BaseRobotMethods(hardwareMap);
+        robot.telemetry = this.telemetry;
+        robot.parent = this;
+
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(startposx, startposy, startheading));
+
         //CAMERA INITIALIZATION --------------------------------------------------------------------
+        telemetry.addData("--Frostbite Close Auto--",true);
+        telemetry.addData("Placement: ", robot.visionProcessor.getSelection());
 
         //EXECUTE ACTIONS -----------------------------------------------------------------
-
         waitForStart();
-        Actions.runBlocking(
-                drive.actionBuilder(drive.pose)
-                        .lineToY(24)
-                        //.splineTo(new Vector2d(startposx+24,startposy+24), startheading)
-                        .build());
+        while(opModeIsActive() && !isStopRequested()) {
+            if (robot.visionProcessor.getSelection() == FirstVisionProcessor.Selected.MIDDLE) {
+                tagposy = -24;
+            }else if(robot.visionProcessor.getSelection() == FirstVisionProcessor.Selected.LEFT){
+                tagposy = -48;
+            }else{
+                tagposy = -72;
+            }
+            Actions.runBlocking(
+                    drive.actionBuilder(drive.pose)
+                            .lineToY(tagposy)
+                            //.splineTo(new Vector2d(startposx+24,startposy+24), startheading)
+                            .build());
+        }
 
     }
 }
