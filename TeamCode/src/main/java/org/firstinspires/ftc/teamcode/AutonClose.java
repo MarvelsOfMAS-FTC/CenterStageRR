35825f48e5afeeab0a10b35e17412f01be57fc03
NoahMarvelsofMAS <noahrahman9@gmail.com>
2023-11-23 22:21:08 -0600

    center purple and yellow done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClose.java
@@ -48,6 +48,7 @@ public class AutonClose extends LinearOpMode {
         robot.climbr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.home(1);
         robot.finger.setPosition(0.37);
+
         //EXECUTE ACTIONS -----------------------------------------------------------------
         waitForStart();
         while (opModeIsActive() && !isStopRequested()) {
@@ -61,71 +62,35 @@ public class AutonClose extends LinearOpMode {
             //}
             robot.elbowl.setPosition(0.29 + elbowHome);//  INTAKE UP // Transfer
             robot.elbowr.setPosition(0.25 + elbowHome);
+
+            //DRIVE TO SCORE SPIKE
             Actions.runBlocking(
                     drive.actionBuilder(drive.pose)
-                            .lineToY(64)
+                            .lineToY(58)
                             .turn(tagheading)
                             .build());
-            telemetry.addLine("0");
-            robot.extend(510);
-            robot.extend.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.extend.setPower(1);
-            sleep(1000);
-            telemetry.addLine("1");
-            telemetry.update();
-            robot.extend.setPower(0);
-            robot.climbl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.climbl.setPower(1);
-            robot.climbr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.climbr.setPower(1);
-            sleep(1000);
-            robot.finger.setPosition(0.84);
-            telemetry.addLine("2");
-            telemetry.update();
-            robot.climbl.setPower(0);
-            robot.climbr.setPower(0);
-            telemetry.addLine("3");
-            telemetry.update();
-            robot.home(1);
-            robot.extend.setTargetPosition(0);
-            robot.extend.setPower(-0.5);
-            robot.extend.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            sleep(1000);
-            robot.extend.setPower(0);
-            robot.home(1);
+            robot.spikeExtend(510);
+            sleep(500);
+            robot.retract(1);
+
+            //SPLINE TO BACKBOARD PREP POS
             Actions.runBlocking(
-                    drive.actionBuilder(new Pose2d(0,64,0))
-                            .splineTo(new Vector2d(36, 32), Math.toRadians(180))
+                    drive.actionBuilder(new Pose2d(0,58, Math.toRadians(90)))
+                            .splineToLinearHeading(new Pose2d(36, 32, Math.toRadians(180)), Math.toRadians(90))
                             .build());
-            robot.extend.setTargetPosition(450);
-            robot.climbl.setTargetPosition(280);
-            robot.climbr.setTargetPosition(280);
-            robot.score.setPosition(0.38);
-            robot.extend.setPower(1);
-            robot.climbl.setPower(1);
-            robot.climbr.setPower(1);
-            robot.extend.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.climbl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.climbr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.low(450);
+
+            //DRIVE INTO BACKBOARD TO SCORE
             Actions.runBlocking(
                     drive.actionBuilder(new Pose2d(36, 32, Math.toRadians(180)))
                             .lineToX(53)
                             .waitSeconds(1)
                             .lineToX(45)
                             .build());
-            robot.climbl.setTargetPosition(525);
-            robot.climbr.setTargetPosition(525);
-            robot.climbl.setPower(1);
-            robot.climbr.setPower(1);
-            robot.climbl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.climbr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.extend.setTargetPosition(0);
-            robot.extend.setPower(-0.5);
-            robot.extend.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            sleep(1000);
-            robot.extend.setPower(0);
-            robot.home(1);
+            robot.mid(450);
+            robot.retract(1);
 
+            //DRIVE TO PUCK STACK
             Actions.runBlocking(
                     drive.actionBuilder(new Pose2d(45, 32, Math.toRadians(180)))
                             .lineToX(-60)
