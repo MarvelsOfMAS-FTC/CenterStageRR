750d3bf2b1e61447fe3c5eed52363dc04acd04b0
marvelsofmas <108832774+marvelsofmas@users.noreply.github.com>
2023-11-26 20:53:45 -0600

    cycling auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClose.java
@@ -1,108 +1,155 @@
-//package org.firstinspires.ftc.teamcode;
-//import static com.acmerobotics.roadrunner.ftc.Actions.runBlocking;
-//
-//import android.hardware.HardwareBuffer;
-//
-//import androidx.annotation.NonNull;
-//
-//import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-//import com.acmerobotics.roadrunner.Action;
-//import com.acmerobotics.roadrunner.Line;
-//import com.acmerobotics.roadrunner.ParallelAction;
-//import com.acmerobotics.roadrunner.Pose2d;
-//import com.acmerobotics.roadrunner.SequentialAction;
-//import com.acmerobotics.roadrunner.Vector2d;
-//import com.acmerobotics.roadrunner.ftc.Actions;
-//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.HardwareMap;
-//
-//@Autonomous
-//public class AutonClose extends LinearOpMode {
-//    //VARIABLES---------------------------------------------------------------------------------------------------------------
-//    double startposx = 9;
-//    double startposy = 72;
-//    double startheading = Math.toRadians(90);
-//    double tagposx=0;
-//    double tagposy=-5;
-//    double tagheading = Math.toRadians(1);
-//
-//    double elbowHome = 0.0;
-//
-//    @Override
-//    public void runOpMode() throws InterruptedException {
-//        //ROBOT INITIALIZATION ---------------------------------------------------------------------
-//        BaseRobotMethods robot = new BaseRobotMethods(hardwareMap);
-//
-//        robot.telemetry = this.telemetry;
-//        robot.parent = this;
-//
-//        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(startposx, startposy, startheading));
-//
-//        //CAMERA INITIALIZATION --------------------------------------------------------------------
-//        telemetry.addData("--Frostbite Close Auto--", true);
-//        telemetry.addData("Placement: ", robot.visionProcessor.getSelection());
-//        telemetry.update();
-//        robot.climbl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        robot.climbr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        robot.home();
-//        robot.finger.setPosition(0.37);
-//
-//        //EXECUTE ACTIONS -----------------------------------------------------------------
-//        waitForStart();
-//        while (opModeIsActive() && !isStopRequested()) {
-//            tagposy = -5;
-//            //if (robot.visionProcessor.getSelection() == FirstVisionProcessor.Selected.MIDDLE) {
-//            //    tagheading = 1;
-//            //} else if (robot.visionProcessor.getSelection() == FirstVisionProcessor.Selected.LEFT) {
-//            //    tagheading=30;
-//            //} else {
-//            //    tagheading=-30;
-//            //}
-//            robot.elbowl.setPosition(0.29 + elbowHome);//  INTAKE UP // Transfer
-//            robot.elbowr.setPosition(0.25 + elbowHome);
-//
-//            //DRIVE TO SCORE SPIKE
-//            Actions.runBlocking(
-//                    drive.actionBuilder(drive.pose)
-//                            .lineToY(58)
-//                            .turn(tagheading)
-//                            .build());
-//            robot.spikeExtend(510);
-//            sleep(500);
-//            robot.home();
-//
-//            //SPLINE TO BACKBOARD PREP POS
-//            Actions.runBlocking(
-//                    drive.actionBuilder(new Pose2d(0,58, Math.toRadians(90)))
-//                            .splineToLinearHeading(new Pose2d(36, 32, Math.toRadians(180)), Math.toRadians(90))
-//                            .build());
-//            robot.low();
-//
-//            //DRIVE INTO BACKBOARD TO SCORE
-//            Actions.runBlocking(
-//                    drive.actionBuilder(new Pose2d(36, 32, Math.toRadians(180)))
-//                            .lineToX(53)
-//                            .waitSeconds(1)
-//                            .lineToX(45)
-//                            .build());
-//            robot.mid();
-//            robot.home();
-//
-//            //DRIVE TO PUCK STACK
-//            Actions.runBlocking(
-//                    drive.actionBuilder(new Pose2d(45, 32, Math.toRadians(180)))
-//                            .lineToX(-60)
-//                            .build());
-//
-//            break;
-//
-//
-//        }
-//
-//
-//    }
-//
-//}
-//
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous
+public class AutonClose extends LinearOpMode {
+    //VARIABLES---------------------------------------------------------------------------------------------------------------
+    public String side = "None";
+    public String placement = "None"; //this is the variable for which spot the robot should score
+    public boolean grabStack = true; //grabstack means robot will go pick up 2 pixels and deposit
+
+    //START POS
+    double startposx = 12;
+    double startposy = 72;
+    double startheading = Math.toRadians(90);
+
+    //TAG POS
+    double tagheading = Math.toRadians(90);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //ROBOT INITIALIZATION ---------------------------------------------------------------------
+        BaseRobotMethods robot = new BaseRobotMethods(hardwareMap);
+        robot.telemetry = this.telemetry;
+        robot.parent = this;
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(startposx, startposy, startheading));
+        drive.updatePoseEstimate();
+
+        //CAMERA INITIALIZATION --------------------------------------------------------------------
+        telemetry.addData("Placement: ", robot.visionProcessor.getSelection());
+        telemetry.update();
+
+        //WAIT FOR START CODE ----------------------------------------------------------------------
+        while (!opModeIsActive() && !isStopRequested())
+        {
+            //Running pipeline
+            if(gamepad1.x) {
+                side = "Blue";
+            } else if(gamepad1.b) {
+                side = "Red";
+            }
+
+            //determine if we should grab the stack
+            if(gamepad1.a) {
+                grabStack = true;
+            } else if(gamepad1.y) {
+                grabStack = false;
+            }
+
+            telemetry.addData("--Frostbite Close Auto--",true);
+            telemetry.addData("Placement: ", robot.visionProcessor.getSelection());
+            telemetry.addData("Side: ", side);
+            telemetry.addData("GrabStack?: ", grabStack);
+
+            telemetry.addData("Press X for Blue Side",true);
+            telemetry.addData("Press B for Red Side",true);
+            telemetry.addData("Press A for Grabbing Stack",true);
+            telemetry.addData("Press Y for NOT Grabbing Stack",true);
+            telemetry.update();
+        }
+
+        //EXECUTE ACTIONS -----------------------------------------------------------------
+        while (opModeIsActive() && !isStopRequested()) {
+
+            //SELECT TEAM ELEMENT SIDE
+            if (robot.visionProcessor.getSelection() == FirstVisionProcessor.Selected.MIDDLE) {
+                tagheading = Math.toRadians(100);
+            } else if (robot.visionProcessor.getSelection() == FirstVisionProcessor.Selected.LEFT) {
+                tagheading = Math.toRadians(100); //130
+            } else {
+                tagheading = Math.toRadians(100); //60
+            }
+
+            //SCORE SPIKE MARK PIXEL & DRIVE TO BACKDROP
+            Action spikeMark = drive.actionBuilder(drive.pose)
+                    //SCORE MARK PIXEL
+                    .afterTime(0, robot.spikeExtend())
+                    .afterTime(1, robot.spikeScore())
+                    .afterTime(1.25, robot.fingerHome())
+                    .afterTime(1.5, robot.home())
+
+                    //PREPARE BACKDROP PIXEL
+                    .afterTime(3, robot.low())
+
+                    //THE HEADING IS CONTROLLED BY THE VISION CODE
+                    .lineToYLinearHeading(55, tagheading)
+                    .waitSeconds(0.5)
+                    .splineToLinearHeading(new Pose2d(36, 38, Math.toRadians(180)), Math.toRadians(tagheading))
+                    .build();
+
+            Actions.runBlocking(spikeMark);
+            drive.updatePoseEstimate();
+
+            //SCORE BACKDROP PIXEL
+            Action backDrop = drive.actionBuilder(drive.pose)
+                    //CLEAR PIXEL AFTER SCORING
+                    .afterTime(0.5, robot.mid())
+
+                    //PUSH INTO BACKDROP & SCORE
+                    .lineToX(44)
+                    .build();
+
+            Actions.runBlocking(backDrop);
+            drive.updatePoseEstimate();
+
+            //CYCLE PIXEL STACK
+            Action cyclePixel = drive.actionBuilder(drive.pose)
+                    //TUCK IN SCORE BUCKET & WHIP OUT INTAKE
+                    .afterTime(0, robot.home())
+                    .afterTime(1.75, robot.intakeLevel5())
+                    .afterTime(2.75, robot.intakeUp())
+                    .afterTime(3.5, robot.transfer())
+                    .afterTime(5.5, robot.mid())
+
+                    //GOTO STACK AND RETURN
+                    .lineToXLinearHeading(-53, Math.toRadians(178))
+                    .waitSeconds(0.5)
+                    .lineToXLinearHeading(44, Math.toRadians(180))
+                    .build();
+
+            Actions.runBlocking(cyclePixel);
+            drive.updatePoseEstimate();
+
+            Action cyclePixel1 = drive.actionBuilder(drive.pose)
+                    //TUCK IN SCORE BUCKET & WHIP OUT INTAKE
+                    .afterTime(0, robot.home())
+                    .afterTime(1.75, robot.intakeGround())
+                    .afterTime(2.75, robot.intakeUp())
+                    .afterTime(3.5, robot.transfer())
+                    .afterTime(5.5, robot.mid())
+
+                    //GOTO STACK AND RETURN
+                    .lineToXLinearHeading(-55, Math.toRadians(176))
+                    .waitSeconds(0.5)
+                    .lineToXLinearHeading(44, Math.toRadians(180))
+                    .build();
+
+            Actions.runBlocking(cyclePixel1);
+            drive.updatePoseEstimate();
+            break;
+        }
+    }
+
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseRobotMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseRobotMethods.java
@@ -18,91 +18,23 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorControllerEx;
-import com.qualcomm.robotcore.hardware.DcMotorController;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.LED;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.TouchSensor;
-
-import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
-import android.util.Size;
-
-import com.qualcomm.hardware.lynx.LynxModule;
-import com.qualcomm.hardware.lynx.commands.core.LynxGetADCCommand;
-import com.qualcomm.hardware.lynx.commands.core.LynxGetADCResponse;
-import com.qualcomm.hardware.lynx.LynxNackException;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.Range;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.IMU;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 
 public class BaseRobotMethods{
 
     //VARIABLES---------------------------------------------------------------------------------------------------------------
     private static final boolean USE_WEBCAM = true;
-    double DESIRED_DISTANCE = 20.0; //  this is how close the camera should get to the target (inches)
-    private int DESIRED_TAG_ID = 5;     // Choose the tag you want to approach or set to -1 for ANY tag.
-    private VisionPortal visionPortal;               // Used to manage the video source.
-    private AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
-    private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
-
-    static RevHubOrientationOnRobot.LogoFacingDirection[] logoFacingDirections
-            = RevHubOrientationOnRobot.LogoFacingDirection.values();
-    static RevHubOrientationOnRobot.UsbFacingDirection[] usbFacingDirections
-            = RevHubOrientationOnRobot.UsbFacingDirection.values();
-    static int LAST_DIRECTION = logoFacingDirections.length - 1;
-    static float TRIGGER_THRESHOLD = 0.2f;
-
-    double aprilTagX = 0.0;
-    double aprilTagY = 0.0;
-    double aprilTagXError = 999.0;
-    double aprilTagYError = 999.0;
-    double xPosition = 0.0;
-    double oldAprilTagSum = 0.0;
-    double aprilTagSum = 0.0;
-
-    boolean tagChange = true;
-    boolean autoAim = false;
-    boolean targetFound = false;
-
-    IMU imu;
-    int logoFacingDirectionPosition;
-    int usbFacingDirectionPosition;
-    boolean orientationIsValid = true;
     ElapsedTime timer = new ElapsedTime();
     boolean holdWrist = false;
     boolean resetEncoder = false;
+
+    private AprilTagProcessor aprilTag;
+
     public FirstVisionProcessor visionProcessor;
+
+    private VisionPortal visionPortal;
+    private VisionPortal visionPortalBack;
+
     private VisionPortal visionPortalApril;
 
     DcMotorEx intake, climbl, climbr, extend, fl, fr, bl, br;
@@ -182,158 +114,18 @@ public class BaseRobotMethods{
         //CAMERA INIT
         visionProcessor = new FirstVisionProcessor();
         initCamera(hardwareMap);
-
-        imu = hardwareMap.get(IMU.class, "imu");
-        logoFacingDirectionPosition = 0; // Up
-        usbFacingDirectionPosition = 2; // Forward
-
-        updateOrientation();
-
-        boolean justChangedLogoDirection = false;
-        boolean justChangedUsbDirection = false;
-
-        boolean targetFound = false;    // Set to true when an AprilTag target is detected
-
-        if (USE_WEBCAM) setManualExposure(3, 250);
-
-        visionPortal.setProcessorEnabled(visionProcessor, true);
     }
 
     //CAMERA COMMANDS ----------------------------------------------------------------------------------------------------
     public void initCamera(HardwareMap hardwareMap){
-        aprilTag = new AprilTagProcessor.Builder().build();
-        aprilTag.setDecimation(1);
-
         visionPortal = new VisionPortal.Builder()
                 .setCamera(hardwareMap.get(WebcamName.class, "webcamback"))
-                .addProcessors(visionProcessor,aprilTag)
-                .setCameraResolution(new Size(640, 360)) // 544 288
+                .addProcessor(visionProcessor)
+                .setCameraResolution(new Size(864, 480)) // 544 288
                 .setStreamFormat(VisionPortal.StreamFormat.YUY2)
                 .setAutoStopLiveView(true)
                 .build();
     }
-
-    public void aprilTagTrack(int trackLocation)
-    {
-        visionPortal.setProcessorEnabled(visionProcessor, false);
-        visionPortal.setProcessorEnabled(aprilTag, true);
-        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-        AngularVelocity angularVelocity = imu.getRobotAngularVelocity(AngleUnit.DEGREES);
-
-        double headingStart = Math.round(orientation.getYaw(AngleUnit.DEGREES)/90) * 90;
-        double headingError = 0.0;
-
-        while(!parent.isStopRequested() && (Math.abs(aprilTagYError) < 0.25 || Math.abs(aprilTagXError) < 0.25)) {
-            targetFound = false;
-            desiredTag = null;
-
-            // Step through the list of detected tags and look for a matching tag
-            List<AprilTagDetection> currentDetections = aprilTag.getDetections();
-            for (AprilTagDetection detection : currentDetections) {
-                // Look to see if we have size info on this tag.
-                if (detection.metadata != null) {
-
-                    int detectionId = detection.id;
-
-                    //make sure we don't auto track the other april tags
-                    if (detectionId > 6) {
-                        targetFound = false;
-                    } else {
-                        targetFound = true;
-                    }
-
-                    if (detection.id > 3) {
-                        detectionId = detectionId - 3;
-                    }
-                    aprilTagY = aprilTagY + detection.ftcPose.y;
-                    aprilTagX = aprilTagX + (-(detectionId - 2) * 3.8) + detection.ftcPose.x;
-                    telemetry.addData("Found: ", "Tag ID %d", detectionId);
-                } else {
-                    // This tag is NOT in the library, so we don't have enough information to track to it.
-                    telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
-                }
-            }
-
-            aprilTagY = aprilTagY / currentDetections.size();
-            aprilTagX = aprilTagX / currentDetections.size();
-
-            autoAim = false;
-            if (targetFound && aprilTagY < 50.0) {
-                autoAim = true;
-            }
-
-            if (autoAim) {
-
-                headingError = orientation.getYaw(AngleUnit.DEGREES) - headingStart;
-
-                xPosition = (trackLocation-2)*6;
-
-                double aprilTagXError = -(aprilTagX - xPosition);
-
-                if (aprilTagY < 26 && extend.getCurrentPosition() > 200) {
-                    DESIRED_DISTANCE = 13 + Math.abs(aprilTagXError);
-                } else {
-                    DESIRED_DISTANCE = 25;
-                }
-
-                double aprilTagYError = -(aprilTagY - DESIRED_DISTANCE);
-
-
-                if (autoAim) {
-                    aprilTagSum = aprilTagX + aprilTagY;
-                    if (aprilTagSum != oldAprilTagSum) {
-
-                        if (Math.abs(orientation.getYaw(AngleUnit.DEGREES)) < 10) {
-                            double forwardTicks = 8;
-
-                            double strafeTicks = 10;
-
-                            if (aprilTagXError < 5 && fl.getVelocity() < 100) {
-                                strafeTicks = 10;
-                            }
-                            fl.setTargetPosition(fl.getCurrentPosition() + (int) (aprilTagYError * forwardTicks) + (int) (aprilTagXError * strafeTicks) - (int) (headingError * 6.5));
-                            fr.setTargetPosition(fr.getCurrentPosition() + (int) (aprilTagYError * forwardTicks) - (int) (aprilTagXError * strafeTicks) + (int) (headingError * 6.5));
-                            bl.setTargetPosition(bl.getCurrentPosition() + (int) (aprilTagYError * forwardTicks) - (int) (aprilTagXError * strafeTicks) - (int) (headingError * 6.5));
-                            br.setTargetPosition(br.getCurrentPosition() + (int) (aprilTagYError * forwardTicks) + (int) (aprilTagXError * strafeTicks) + (int) (headingError * 6.5));
-
-                        } else {
-                            fl.setTargetPosition(fl.getCurrentPosition() - (int) (headingError * 6.5));
-                            fr.setTargetPosition(fr.getCurrentPosition() + (int) (headingError * 6.5));
-                            bl.setTargetPosition(bl.getCurrentPosition() - (int) (headingError * 6.5));
-                            br.setTargetPosition(br.getCurrentPosition() + (int) (headingError * 6.5));
-                        }
-
-                        fl.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-                        fr.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-                        bl.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-                        br.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-
-                        double motorPower = 1.0;
-                        fl.setPower(motorPower);
-                        fr.setPower(motorPower);
-                        bl.setPower(motorPower);
-                        br.setPower(motorPower);
-                    }
-                }
-
-            }
-            aprilTagY = 0.0;
-            aprilTagX = 0.0;
-            oldAprilTagSum = aprilTagSum;
-
-            // Tell the driver what we see, and what to do.
-            if (targetFound) {
-                telemetry.addData("Average Y Value: ", aprilTagY);
-                telemetry.addData("Average X Value: ", aprilTagX);
-                telemetry.addData("Heading Start", headingStart);
-                telemetry.addData("Heading Error: ", headingError);
-            }
-
-            telemetry.update();
-        }
-    }
-
-
 
     public void stopMovement(){
         fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -367,9 +159,9 @@ public class BaseRobotMethods{
     public class IntakeGround implements Action{
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-            elbowl.setPosition(0.59);//  INTAKE DOWN and TURN ON
-            elbowr.setPosition(0.55);
-            wrist.setPosition(0.3);
+            elbowl.setPosition(0.5973);//  INTAKE DOWN and TURN ON
+            elbowr.setPosition(0.5574);
+            wrist.setPosition(0.32);
 
             intake.setPower(-1.0); //turn intake on full speed
 
@@ -379,6 +171,23 @@ public class BaseRobotMethods{
     }
     public Action intakeGround() {
         return new IntakeGround();
+    }
+
+    public class IntakeLevel5 implements Action{
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            elbowl.setPosition(0.555);//  INTAKE DOWN and TURN ON
+            elbowr.setPosition(0.5153);
+            wrist.setPosition(0.34);
+
+            intake.setPower(-1.0); //turn intake on full speed
+
+            passiveIntake = true;
+            return false;
+        }
+    }
+    public Action intakeLevel5() {
+        return new IntakeLevel5();
     }
 
     public class Transfer implements Action{
@@ -406,9 +215,9 @@ public class BaseRobotMethods{
     public class IntakeUp implements Action{
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-            elbowl.setPosition(0.29 + elbowHome);
-            elbowr.setPosition(0.25 + elbowHome);
-            wrist.setPosition(0.725);
+            elbowl.setPosition(0.34122 + elbowHome);
+            elbowr.setPosition(0.30122 + elbowHome);
+            wrist.setPosition(0.744);
             return false;
         }
 
@@ -484,83 +293,6 @@ public class BaseRobotMethods{
     }
     public Action mid(){
         return new Mid();
-    }
-
-
-    public class IntakeToPos implements Action {
-        public Action init(int pos){
-            if (pos == 5) {
-                //wrist.setPosition(0.485);
-                //elbowl.setPosition(0.95);
-                //elbowr.setPosition(0.35);
-            } else if (pos == 4) {
-                //wrist.setPosition(0.455);
-                //elbowl.setPosition(0.965);
-                //elbowr.setPosition(0.335);
-            } else if (pos == 3) {
-                //wrist.setPosition(0.475);
-                //elbowl.setPosition(0.965);
-                //elbowr.setPosition(0.335);
-            } else if (pos == 2) {
-                //wrist.setPosition(0.42);
-                //elbowl.setPosition(0.965);
-                //elbowr.setPosition(0.335);
-            }
-            return new IntakeToPos();
-        }
-        @Override
-        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-            return false;
-        }
-    }
-    public Action intakeToPos(int pos){
-        return new IntakeToPos().init(pos);
-    }
-
-    private void setManualExposure(int exposureMS, int gain) {
-        // Wait for the camera to be open, then use the controls
-
-        if (visionPortal == null) {
-            return;
-        }
-
-        // Make sure camera is streaming before we try to set the exposure controls
-        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
-            telemetry.addLine("Camera Waiting");
-            telemetry.update();
-            while (!parent.isStopRequested() && (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
-                parent.sleep(20);
-            }
-            telemetry.addData("Camera", "Ready");
-            telemetry.update();
-        }
-
-        // Set camera controls unless we are stopping.
-        if (!parent.isStopRequested())
-        {
-            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
-            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
-                exposureControl.setMode(ExposureControl.Mode.Manual);
-                parent.sleep(50);
-            }
-            exposureControl.setExposure((long)exposureMS, TimeUnit.MILLISECONDS);
-            parent.sleep(20);
-            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
-            gainControl.setGain(gain);
-            parent.sleep(20);
-        }
-    }
-
-    void updateOrientation() {
-        RevHubOrientationOnRobot.LogoFacingDirection logo = logoFacingDirections[logoFacingDirectionPosition];
-        RevHubOrientationOnRobot.UsbFacingDirection usb = usbFacingDirections[usbFacingDirectionPosition];
-        try {
-            RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logo, usb);
-            imu.initialize(new IMU.Parameters(orientationOnRobot));
-            orientationIsValid = true;
-        } catch (IllegalArgumentException e) {
-            orientationIsValid = false;
-        }
     }
 
     public static double Tiles(double amt_of_tiles){
