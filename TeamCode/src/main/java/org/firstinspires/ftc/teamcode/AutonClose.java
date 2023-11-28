47de6e877bd563717e8ce8fa655632ca08bfd3ac
Marvels of Mas <marvelsofmas@gmail.com>
2023-11-27 20:37:18 -0600

    ISAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQ

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonClose.java
@@ -78,10 +78,10 @@ public class AutonClose extends LinearOpMode {
                 tagheading = Math.toRadians(100);
                 tagoffset = 0;
             } else if (robot.visionProcessor.getSelection() == FirstVisionProcessor.Selected.LEFT) {
-                tagheading = Math.toRadians(130); //130
+                tagheading = Math.toRadians(125); //130
                 tagoffset = 4;
             } else {
-                tagheading = Math.toRadians(60); //60
+                tagheading = Math.toRadians(75); //60
                 tagoffset = -4;
             }
 
