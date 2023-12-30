72043f70c40731c82341637f5b322138568a6036
IKKNIGHT <76535471+IKKNIGHT@users.noreply.github.com>
2023-12-30 16:14:13 -0600

    BaseRobotMethods

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseConstants.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Line;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Config
+@Autonomous
+public class BaseConstants extends LinearOpMode {
+    public static final double RIGHT_CLAW_OPEN = -0.7;
+    public static final double LEFT_CLAW_OPEN = 1;
+    public static final double RIGHT_CLAW_CLOSE = 0.5;
+    public static final double LEFT_CLAW_CLOSE = 0.2;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseRobotMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseRobotMethods.java
@@ -4,27 +4,30 @@ import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Actions;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class BaseRobotMethods {
+    BaseConstants $ = new BaseConstants();
     boolean rightclose = false;
     boolean leftclose = false;
     boolean changedRight = false;
     boolean changedLeft = false;
     Servo wrist, leftclaw, rightclaw, hook, drone;
-    DcMotor arml, armr, sliders, hanging;
+    DcMotorEx arml, armr, sliders, hanging;
 
     public BaseRobotMethods(HardwareMap hardwareMap) {
         // initialise
         wrist = hardwareMap.servo.get("wrist");
         leftclaw = hardwareMap.servo.get("right");
         rightclaw = hardwareMap.servo.get("left");
-        arml = hardwareMap.dcMotor.get("ArmL");
-        armr = hardwareMap.dcMotor.get("ArmR");
-        sliders = hardwareMap.dcMotor.get("Slider");
-        hanging = hardwareMap.dcMotor.get("hanging");
+        arml = hardwareMap.get(DcMotorEx.class,"arml");
+        armr = hardwareMap.get(DcMotorEx.class, "armr");
+        sliders = hardwareMap.get(DcMotorEx.class, "sliders");
+        hanging = hardwareMap.get(DcMotorEx.class, "hanging");
         hook = hardwareMap.servo.get("hook");
         drone = hardwareMap.servo.get("drone");
         arml.setDirection(DcMotor.Direction.REVERSE);
@@ -42,24 +45,103 @@ public class BaseRobotMethods {
 
 
     }
-    public class rightClaw implements Action{
+
+    public class OpenClaw implements Action {
         @Override
-        public boolean run(@NonNull TelemetryPacket telemetryPacket){
-            rightclaw.setPosition(-0.7); // Open right claw
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            rightclaw.setPosition($.RIGHT_CLAW_OPEN); // Open right claw
+            rightclose = false;
+            leftclaw.setPosition($.LEFT_CLAW_OPEN); // Open left claw
+            leftclose = false;
+            return false;
+        }
+    }
+
+    public Action openClaw() {
+        return new OpenClaw();
+    }
+
+    public class CloseClaw implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            rightclaw.setPosition($.RIGHT_CLAW_CLOSE); // Close right claw
+            rightclose = true;
+            leftclaw.setPosition($.LEFT_CLAW_CLOSE); // Close left claw
+            leftclose = true;
+            return false;
+        }
+    }
+
+    public Action closeClaw() {
+        return new CloseClaw();
+    }
+
+    public class OpenRightClaw implements Action {
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            rightclaw.setPosition($.RIGHT_CLAW_OPEN); // Open right claw
             rightclose = false;
             return false;
         }
     }
-    public Action rightClaw(){
-        return new rightClaw();
+
+    public Action openRightClaw() {
+        return new OpenRightClaw();
     }
 
+    public class OpenLeftClaw implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            leftclaw.setPosition($.LEFT_CLAW_OPEN);
+            leftclose = false;
+            return false;
+        }
+    }
+
+    public Action openLeftClaw() {
+
+        return new OpenLeftClaw();
+
+    }
+
+    public class CloseRightClaw implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            rightclaw.setPosition($.RIGHT_CLAW_CLOSE); // Close right claw
+            rightclose = true;
+            return false;
+        }
+    }
+
+    public Action CloseRightClaw() {
+        return new CloseRightClaw();
+    }
+
+    public class CloseLeftClaw implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            leftclaw.setPosition($.LEFT_CLAW_CLOSE); // Close right claw
+            leftclose = true;
+            return false;
+        }
+    }
+
+    public Action CloseLeftClaw() {
+        return new CloseLeftClaw();
+    }
+
+    public class Low implements Action  {
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            sliders.setTargetPosition(2000);
+            sliders.setPower(1);
+            sliders.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            return false;
+        }
+    }
 }
-
-
-
-
-
 
 
 
