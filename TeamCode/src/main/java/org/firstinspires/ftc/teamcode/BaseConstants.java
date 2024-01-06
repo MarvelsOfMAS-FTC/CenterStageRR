9814ff7dde072935619af97f2d0582b11785fa4a
IKKNIGHT <76535471+IKKNIGHT@users.noreply.github.com>
2024-01-05 20:16:20 -0600

    Update BaseConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseConstants.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Config
-public class BaseConstants extends LinearOpMode {
+public class BaseConstants{
     public double RIGHT_CLAW_OPEN = -0.7;
     public double LEFT_CLAW_OPEN = 1;
     public double RIGHT_CLAW_CLOSE = 0.5;
@@ -17,9 +17,4 @@ public class BaseConstants extends LinearOpMode {
     public  double WRIST_TEST_POSITION = 0.4;
     public  double WRIST_SCORE_POSITION = 0;
     public  double WRIST_PLACE_POSITION=0;
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-        waitForStart();
-    }
 }
