6a4b8a9aca6a99df9a9e19857619819d9c8a2619
IKKNIGHT <76535471+IKKNIGHT@users.noreply.github.com>
2024-01-05 20:03:33 -0600

    Update BaseConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseConstants.java
@@ -6,18 +6,17 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Config
-@Autonomous
 public class BaseConstants extends LinearOpMode {
-    public static final double RIGHT_CLAW_OPEN = -0.7;
-    public static final double LEFT_CLAW_OPEN = 1;
-    public static final double RIGHT_CLAW_CLOSE = 0.5;
-    public static final double LEFT_CLAW_CLOSE = 0.2;
-    public static final int LOW = 2000;
-    public static final int LOW_HOME = 0;
-    public static final double LOW_SPEED = 0.8;
-    public static final double WRIST_TEST_POSITION = 0.4;
-    public static final double WRIST_SCORE_POSITION = 0;
-    public static final double WRIST_PLACE_POSITION=0;
+    public double RIGHT_CLAW_OPEN = -0.7;
+    public double LEFT_CLAW_OPEN = 1;
+    public double RIGHT_CLAW_CLOSE = 0.5;
+    public  double LEFT_CLAW_CLOSE = 0.2;
+    public  int LOW = 2000;
+    public  int LOW_HOME = 0;
+    public  double LOW_SPEED = 0.8;
+    public  double WRIST_TEST_POSITION = 0.4;
+    public  double WRIST_SCORE_POSITION = 0;
+    public  double WRIST_PLACE_POSITION=0;
 
     @Override
     public void runOpMode() throws InterruptedException {
